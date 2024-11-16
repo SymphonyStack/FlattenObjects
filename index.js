47dd6a2746a4b245a95851574874fadef93c3f9b
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 20:10:32 +0530

    ft: add method to parse json and flatten

---
+++ index.js
@@ -0,0 +1,30 @@
+let parsed = "";
+
+const args = process.argv.slice(2);
+const arr = [args[0]];
+
+const parse = (arr, count) => {
+  if (
+    arr === undefined ||
+    arr === null ||
+    arr.length == 0 ||
+    typeof arr !== "object"
+  ) {
+    return;
+  }
+  for (objectK of Object.entries(arr)) {
+    if (typeof objectK[1] == "object") {
+      if (!isNaN(objectK[0])) {
+        parse(objectK[1], count);
+      } else {
+        parsed += "\t".repeat(count) + objectK[0] + "\n";
+        parse(objectK[1], count + 1);
+      }
+    } else {
+      parsed += "\t".repeat(count) + objectK + "\n";
+    }
+  }
+};
+
+parse(arr, 0);
+console.log(JSON.stringify(parsed));
